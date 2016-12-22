b7bc7b464c2159e10a96482d1be4550668193c31
E. S <E. S>
2016-12-22 04:05:43 +0200

    changes

---
+++ app/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('myApp', [
+  'ngRoute'
+]).
+config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+  $locationProvider.hashPrefix('!');
+}]);
